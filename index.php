34a85d7357494c306c999b4cb25e5f833af3193d
usbuild <njuzhangqichao@gmail.com>
2013-03-17 01:31:46 +0000

    fix some errors

--- index.php
+++ index.php
@@ -3,58 +3,104 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <title>测试页面</title>
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" />
-        <link rel="stylesheet" href="css/main.css" type="text/css" />
+	<style type="text/css" rel="stylesheet">
+
+body{
+    background-color: #D8D8D8;
+    color: #444;
+}
+.content-container{
+    line-height: 1.7em;
+    padding-top:40px;
+    width: 960px;
+    margin: 0 auto;
+    padding: 80px 50px 20px 50px;
+    background-color: white;
+    box-shadow: 0 2px 6px rgba(100, 100, 100, 0.3);
+}
+.post-content{
+    width: 100%;
+}
+body > .navbar .brand {
+    padding-right: 0;
+    padding-left: 0;
+    margin-left: 20px;
+    float: right;
+    font-weight: bold;
+    color: black;
+    text-shadow: 0 1px 0 rgba(255, 255, 255, .1), 0 0 30px rgba(255, 255, 255, .125);
+    -webkit-transition: all .2s linear;
+    -moz-transition: all .2s linear;
+    transition: all .2s linear;
+}
+</style>
         <script type="text/javascript" charset="utf-8" src="js/jquery-1.9.1.min.js"></script>
         <script type="text/javascript" charset="utf-8" src="bootstrap/js/bootstrap.min.js"></script>
-        <script type="text/javascript" charset="utf-8" src="js/main.js"></script>
-    </head>
-    <body>
-        <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container">
-                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="brand" href="/">测试站点</a>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav">
-                            <li class="active"> <a href="/">主页</a> </li>
-                            <li> <a href="/about.html">关于</a> </li>
-                            <li> <a href="https://github.com/usbuild" target="_blank">GitHub</a> </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="content-container">
-            <div class="row-fluid">
-                <div class="span9">
-                    <textarea name="content" rows="3" class="post-content"></textarea>
-                </div>
-                <div class="span3">
-                    <a href="javascript:;" class="btn btn-large btn-primary" id="post">发表</a>
-                </div>
-            </div>
-        <hr />
-        <div class="post-list">
-<?php
-    $dm = new Distmem();
-    $dm->connect("localhost", 4327);
-    $dm->use("show");
-    $idx = $dm->get("index");
-    if(!is_null($idx)) {
-        foreach($idx as $i) {
-?>
-<div class="alert alert-block alert-info">
-    <?=$dm->get($i)?>
+        <script type="text/javascript" charset="utf-8">
+	$(function() {
+			$("#post").click(function(){
+				var content = $(".post-content").val();
+				$.post("post.php", {data:content}, function(e){
+					if(e.code == 0) {
+					var div = $('<div class="alert alert-block alert-info"></div>');
+					div.html(e.data);
+					$(".post-list").prepend(div);
+					$(".post-content").val("");
+					} else {
+alert(e.data);
+}
+					}, "json");
+				});;
+			});
+</script>
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar-inner">
+<div class="container">
+<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="brand" href="/">测试站点</a>
+<div class="nav-collapse collapse">
+<ul class="nav">
+<li class="active"> <a href="/">主页</a> </li>
+<li> <a href="/about.html">关于</a> </li>
+<li> <a href="https://github.com/usbuild" target="_blank">GitHub</a> </li>
+</ul>
 </div>
+</div>
+</div>
+</div>
+<div class="content-container">
+<div class="row-fluid">
+<div class="span9">
+<textarea name="content" rows="3" class="post-content"></textarea>
+</div>
+<div class="span3">
+<a href="javascript:;" class="btn btn-large btn-primary" id="post">发表</a>
+</div>
+</div>
+<hr />
+<div class="post-list">
 <?php
-    }
+$dm = new Distmem();
+$dm->connect("localhost", 4327);
+$dm->use("show");
+$idx = $dm->get("index");
+if(!is_null($idx)) {
+	foreach($idx as $i) {
+		?>
+			<div class="alert alert-block alert-info">
+			<?=$dm->get($i)?>
+			</div>
+			<?php
+	}
 }
 ?>
-        </div>
-        </div>
-    </body>
+</div>
+</div>
+</body>
 </html>
--- post.php
+++ post.php
@@ -1,17 +1,21 @@
 <?php
 if(isset($_POST['data'])) {
-    $data = strip_tags($_POST['data']);
-    $dm = new Distmem();
-    $dm->connect("localhost", 4327);
-    $dm->use("show");
-    $idx = $dm->get("index");
-    if(is_null($idx)) {
-        $idx = array();
-    }
-    $t = time();
-    array_unshift($idx, $t);
-    $dm->set("index", $idx);
-    $dm->set($t, $data);
-    echo json_encode(array("code"=>0, "data"=>$data));
+	$data = strip_tags($_POST['data']);
+	if(strlen(trim($data)) == 0) {
+		echo json_encode(array("code"=>1, "data"=>"输入内容不能为空"));
+	} else {
+		$dm = new Distmem();
+		$dm->connect("localhost", 4327);
+		$dm->use("show");
+		$idx = $dm->get("index");
+		if(is_null($idx)) {
+			$idx = array();
+		}
+		$t = time();
+		array_unshift($idx, $t);
+		$dm->set("index", $idx);
+		$dm->set($t, $data);
+		echo json_encode(array("code"=>0, "data"=>$data));
+	}
 }
 ?>
